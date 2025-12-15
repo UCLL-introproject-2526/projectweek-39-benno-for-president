9240b040aecca35ce5dd47540b9a4d439ba6a994
Sander Torfs <r1094655@ucll.be>
2025-12-15 15:51:27 +0100

    testing

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import pygame
+
+
 def create_main_surface():
-    while True == True:
+    while True:
         pygame.display.set_mode(screen_size)
 
+def draw_circle_on_surface(surface, color, center, radius):
+    pygame.display.draw(surface = surface, color= color, center = center, radius = radius)
 
 # Initialize Pygame
 pygame.init()
@@ -12,4 +16,7 @@ screen_size = (1024, 768)
 
 # Create window with given size
 
+
+draw_circle_on_surface(pygame.display.set_mode, (255,255,255), (512,348), 300)
 create_main_surface()
+
