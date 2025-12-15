4064efe3c9f62c2f48097f89190ea30fa7d48f1e
Sander Torfs <r1094655@ucll.be>
2025-12-15 14:54:46 +0100

    game file toegevoegd

---
+++ Game.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+# Initialize Pygame
+pygame.init()
+
+# Tuple representing width and height in pixels
+screen_size = (1024, 768)
+
+# Create window with given size
+while True:
+    pygame.display.set_mode(screen_size)
