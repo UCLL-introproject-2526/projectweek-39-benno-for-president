1c5a6c90d4bbda95cf02d6e1559e4e03f8ad9068
Sander Torfs <r1094655@ucll.be>
2025-12-16 09:48:32 +0100

    circlemovement

--- app.py
+++ app.py
@@ -9,17 +9,38 @@ def main():
     clock = pygame.time.Clock()
 
 
+    def render_frame(surface, xpos, ypos):
+        pygame.draw.circle(surface, (255,255,255), (xpos, ypos), 100)
+
+    xpos = 400
+    ypos = 200
+    
     run = True
 
     while run:
-        
+        screen.fill((0,0,0))
+
+
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_d]:
+            xpos += 5
+        if key[pygame.K_q]:
+            xpos -= 5
+        if key[pygame.K_s]:
+            ypos += 5
+        if key[pygame.K_z]:
+            ypos -= 5
+
+
+
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
                 run = False
-      
        
-
+        
+        render_frame(screen, xpos, ypos)
     
 
         clock.tick(60)
