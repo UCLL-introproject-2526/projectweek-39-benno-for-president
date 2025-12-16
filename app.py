98581b620278babb0477d7f8d602c147510d43f8
Sander Torfs <r1094655@ucll.be>
2025-12-16 09:25:57 +0100

    circledraw

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import pygame
-
 pygame.init()
 
 def main():
@@ -8,17 +7,24 @@ def main():
     screen_size = (800,400)
     screen = pygame.display.set_mode(screen_size)
     clock = pygame.time.Clock()
-    circle
+
+
     run = True
 
     while run:
-
+        
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 run = False
+      
+       
 
-    pygame.display.flip()
-    clock.tick(60)
+    
+
+        clock.tick(60)
+        pygame.display.flip()
+    
     pygame.quit()
 
 
